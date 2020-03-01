0544ddbbc50d0585fb962d46b73c90658b3b1b18
Sebastian Krüger <2pi_r2@gmx.de>
2020-03-01 20:59:06 +0100

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,14 @@
+import { Module } from "@nuxt/types";
+
 declare module "vue/types/vue" {
   interface Vue {
     $notification(title: string, options?: NotificationOptions): Notification;
   }
+}
+
+interface Options {
+  requestPermissionImediate?: boolean;
+  baseOptions?: NotificationOptions;
 }
 
 declare module "@nuxt/types" {
@@ -9,9 +16,10 @@ declare module "@nuxt/types" {
     $notification(title: string, options?: NotificationOptions): Notification;
   }
   interface Configuration {
-    "nuxt-notification"?: {
-      requestPermissionImediate?: boolean;
-      baseOptions?: NotificationOptions;
-    };
+    "nuxt-notification"?: Options;
   }
 }
+
+declare const notificationModule: Module<Options>;
+export { Options };
+export default notificationModule;
